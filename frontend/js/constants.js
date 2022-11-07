70991278d10c684b730de83f59a9c5fcc99470bc
k0ngz <k0ngzdeployer@gmail.com>
2022-11-06 20:34:05 -0600

    no

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "0x753923d0cD75Fd989561a174eBA6D1c580158c99"; // Replace with your own contract address
+const contractAddress = "YOUR CONTRACT ADDRESS"; // Replace with your own contract address
 const chain = 'goerli'; // goerli, polygon, or ethereum
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
