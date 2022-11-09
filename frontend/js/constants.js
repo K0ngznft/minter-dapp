83f12e8e1af947f81588a87433cc8a7addecb8ca
k0ngz <k0ngzdeployer@gmail.com>
2022-11-08 20:11:26 -0600

    web3

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,12 +1,12 @@
 const contractAddress = "0x753923d0cD75Fd989561a174eBA6D1c580158c99"; // Replace with your own contract address
 const chain = 'goerli'; // goerli, polygon, or ethereum
 
-const welcome_h1 = "Welcome to the Jungle";
+const welcome_h1 = "The Kongz Have Arrived";
 const welcome_h2 = "Connect to MetaMask to Get Started";
 const welcome_p = '5,000 Kongz from deep in the heart of the jungle who wandered onto the Ethereum Blockchain.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
-const h1_public_mint = "Welcome to the Jungle ";
+const h1_public_mint = "Minting Open";
 const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
 const h2_presale_mint = "Public Minting Countdown";
 const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for updates!";
